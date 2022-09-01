8c90fa4ca6f25ba7ed3dedab3ceaffe6459be46d
Richona <105828822+Richona@users.noreply.github.com>
2022-09-01 14:01:30 -0300

    agregue MD para el CRUD, si no estas logueado no podes crear, editar o eliminar

--- src/routes/products.js
+++ src/routes/products.js
@@ -7,12 +7,13 @@ const productsController = require('../controllers/productsController');
 
 // ************ Middleware Require ************
 const {uploadProducts} = require("../middlewares/uploadFiles")
+const authMiddleware = require("../middlewares/authMiddleware")/* Middleware para no permitir ingresar a vistas si no estamos logueado */
 
 /*** GET ALL PRODUCTS ***/ 
 router.get('/', productsController.index); /* Ruta de productos en general, ubicada en header "Todos los productos" */
 
 /*** CREATE ONE PRODUCT ***/ 
-router.get('/create', productsController.create); /* Ruta para agregar un productos, ubicada en header "vender" */
+router.get('/create', authMiddleware, productsController.create); /* Ruta para agregar un productos, ubicada en header "vender" */
 router.post('/create', uploadProducts.single("file"),productsController.store); /* Ruta para guardar un productor, haciendo click en boton "guardar" */
 
 
@@ -20,12 +21,12 @@ router.post('/create', uploadProducts.single("file"),productsController.store); 
 router.get('/detail/:id', productsController.detail); /* Ruta para cada producto en detalle, haciendo click en un producto accedes. */
 
 /*** EDIT ONE PRODUCT ***/ 
-router.get('/edit/:id', productsController.edit); /* Ruta para editar un producto, boton "editar producto" ubicado en la vista de detalle de cada producto */
+router.get('/edit/:id', authMiddleware, productsController.edit); /* Ruta para editar un producto, boton "editar producto" ubicado en la vista de detalle de cada producto */
 router.put('/update/:id', uploadProducts.single("file"),productsController.update); /* Ruta para guardar la edicion de un producto. haciendo click en "guardar producto" */
 
 
 /*** DELETE ONE PRODUCT***/ 
-router.delete('/delete/:id', productsController.destroy); /* Ruta para eliminar un producto, ubicado en el detalle de cada producto */
+router.delete('/delete/:id', authMiddleware, productsController.destroy); /* Ruta para eliminar un producto, ubicado en el detalle de cada producto */
 
 
 module.exports = router;
